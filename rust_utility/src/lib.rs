2e35b4f314d6d6f69e819509c895e30c80a66f5c
Ilkka Ollakka <ilkka.ollakka@iki.fi>
2022-12-03 13:41:32 +0200

    rust_utility: add function to split out to strings
    mainly for day2 resolution at the moment

--- rust_utility/src/lib.rs
+++ rust_utility/src/lib.rs
@@ -13,6 +13,33 @@ pub enum AOCError {
     FileError(std::io::Error),
 }
 
+pub fn read_file_and_split(filename: &str) -> Result<Vec<(String, String)>, Box<dyn Error>> {
+    let mut parsed_output: Vec<(String, String)> = Vec::new();
+
+    let file = File::open(format!("../{}", filename))?;
+    let mut filebuffer = BufReader::new(file);
+
+    loop {
+        let mut line: String = String::new();
+        let read_amount = filebuffer.read_line(&mut line)?;
+
+        if read_amount == 0 {
+            break;
+        }
+
+        line = (*line.trim()).to_string();
+
+        let mut splitted_output = line.split(" ");
+
+        parsed_output.push((
+            splitted_output.next().unwrap().to_string(),
+            splitted_output.next().unwrap().to_string(),
+        ));
+    }
+
+    return Ok(parsed_output);
+}
+
 pub fn read_daily_input(filename: &str, return_amount: usize) -> Result<Vec<u32>, Box<dyn Error>> {
     let file = File::open(format!("../{}", filename))?;
     let mut filebuffer = BufReader::new(file);
