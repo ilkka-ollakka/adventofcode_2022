7bdd1d6c9f4e3064518543417d61f5d01e86405e
Ilkka Ollakka <ilkka.ollakka@iki.fi>
2022-12-11 14:15:45 +0200

    move mixed up day11/day10 files to correct names

--- python_day11/day11.py
+++ python_day11/day11.py
@@ -1,63 +1,129 @@
 #!/usr/bin/env python3
 
+from math import lcm
+
+
 def datareader(filename: str, translate=str) -> list[str]:
     with open(filename, "r") as fileinput:
         for dataline in fileinput:
             yield translate(dataline.strip())
 
 
-def parse_commands(string: str) -> tuple[int, int]:
-    commands = string.split()
-    if commands[0] == 'noop':
-        return (1, 0)
-    else:
-        return (2, int(commands[1]))
+class Monkey(object):
+
+    def __init__(self):
+        self.items = []
+        self.number = 0
+        self.modulator = 1
+        self.operation = None
+        self.items_inspected = 0
+        self.test_operation = {"dividor": 2,
+                               "false_path": None, "true_path": None}
+
+    # test items and returns list of (target, item)
+    def test_items(self) -> list[tuple[int, int]]:
+        results = []
+        while self.items:
+            item = self.items.pop(0)
+            self.items_inspected += 1
+            item_output = int(self.evaluate(item))
+            # for part 1 change this to int(int(item_output) / 3)
+            item_test_value = int(int(int(item_output)) % self.modulator)
+            if item_test_value % self.test_operation["dividor"] == 0:
+                results.append(
+                    (self.test_operation["true_path"], item_test_value))
+            else:
+                results.append(
+                    (self.test_operation["false_path"], item_test_value))
+        return results
+
+    def evaluate(self, input_value):
+        # print(f"operations {self.operation} with value {input_value}")
+        if self.operation[1] == "*":
+            output = 1
+            for value in [self.operation[0], self.operation[2]]:
+                if value == "old":
+                    value = int(input_value)
+                output *= int(value)
+            result = output
+        else:
+            output = 0
+            for value in [self.operation[0], self.operation[2]]:
+                if value == "old":
+                    value = int(input_value)
+                output += int(value)
+            result = output
+        # print(f"Outcome value {result}")
+        return result
+
+    def __str__(self):
+        return f"monkey {self.number} score: {self.items_inspected} items: {self.items} test: {self.test_operation} operator: {self.operation}"
 
 
-def display_crt(crt_screen: list) -> str:
-    return "\n".join(["".join(x) for x in crt_screen])
+def parse_monkeys(string: str) -> Monkey:
+    monkey = Monkey()
+
+    # print(f"monkey-data: '{string}'")
+
+    for line in string.split('\n'):
+        if line.startswith("Monkey "):
+            number = line.split(' ')[1]
+            monkey.number = int(number.split(':')[0])
+            continue
+        if line.startswith("Starting items:"):
+            items = line.split(':')[1]
+            monkey.items = [int(x) for x in items.split(', ')]
+            continue
+        if line.startswith("Operation:"):
+            operation = line.split("=")[1]
+            monkey.operation = operation.strip().split()
+            continue
+        if line.startswith("Test: "):
+            divisor = int(line.split("by")[1])
+            monkey.test_operation["dividor"] = divisor
+            continue
+        if line.startswith("If true"):
+            true_target = int(line.split(' ')[-1])
+            monkey.test_operation["true_path"] = true_target
+            continue
+        if line.startswith("If false"):
+            false_target = int(line.split(' ')[-1])
+            monkey.test_operation["false_path"] = false_target
+            continue
+
+    return monkey
 
 
 if __name__ == "__main__":
-    data_input = datareader("../day10.txt", parse_commands)
+    data_input = "\n".join(datareader(
+        "../day11.txt", str)).split("\n\n")
 
-    cycle_number = 0
-    register_value = 1
-    drawing_position = (0, 0)
+    monkeys = {}
 
-    cycle_milestones = [x for x in range(40, 241, 40)]
-    cycle_values = []
-    crt_screen = [[" " for x in range(0, 41)] for x in range(0, 7)]
+    for monkey_string in data_input:
+        monkey = parse_monkeys(monkey_string)
+        monkeys[monkey.number] = monkey
 
-    cycle, increment = next(data_input)
+    dividors = []
+    # collect dividors
+    for monkey in monkeys.values():
+        dividors.append(int(monkey.test_operation["dividor"]))
 
-    for cycle_number in range(0, 241):
+    dividor = lcm(*dividors)
 
-        if cycle_number >= cycle_milestones[0]:
-            # print(
-            #    f"Cycle milestone reached {cycle_number} register {register_value}")
-            cycle_values.append(cycle_milestones[0] * register_value)
-            cycle_milestones.pop(0)
-            drawing_position = (drawing_position[0] + 1, 0)
-            if len(cycle_milestones) == 0:
-                break
+    for monkey in monkeys.values():
+        monkey.modulator = dividor
 
-        if (drawing_position[1] - 1) <= register_value <= (drawing_position[1] + 1):
-            # print(
-            #    f"Would hit drawing position {drawing_position} {register_value}")
-            crt_screen[drawing_position[0]][drawing_position[1]] = "#"
-        else:
-            crt_screen[drawing_position[0]][drawing_position[1]] = "."
+    for i in range(0, 10000):
+        for monkey_number in sorted(monkeys.keys()):
+            # print(monkey_number)
+            results = monkeys[monkey_number].test_items()
+            for target, value in results:
+                monkey = monkeys[target]
+                monkey.items.append(value)
 
-        cycle -= 1
-        if cycle == 0:
-            register_value += increment
-            try:
-                cycle, increment = next(data_input)
-            except StopIteration:
-                cycle, increment = (1, 0)  # Inject noop
-        drawing_position = (
-            drawing_position[0], drawing_position[1] + 1)
-
-    print(f"sum of cycle values: {sum(cycle_values)}")
-    print(display_crt(crt_screen))
+    scores = [monkey.items_inspected for monkey in monkeys.values()]
+    scores.sort()
+    print(f"items inspected {scores}")
+    print(f"result score: {scores.pop() * scores.pop()}")
+    print(f"lcm {dividor}")
